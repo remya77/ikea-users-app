caf25c312a13556cd29c408bb27827998f0bcf4e
Remya Ramesan <Remya.Ramesan@ingka.ikea.com>
2024-10-11 13:28:20 +0200

    updated db amd test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,37 @@
 const request = require('supertest');
 const { app, server } = require('../index.js');
 
-describe('Test the root path', () => {
-    test('It should respond with "Hello Ikea!"', async () => {
-        const response = await request(app).get('/');
-        //expect(response.text).toBe('Hello Ikea!');
+let id = 0;
+
+describe('Test the endpoints', () => {
+    test('It should respond with user created', async () => {
+        const response = await request(app).post('/api/users')
+            .send({ name: "Johan Finndahl", isAdmin: true, jobTitle: "Engineering Manager" });
+        id = response.body.id;
+        expect(response.body.name).toBe('Johan Finndahl');
         expect(response.statusCode).toBe(200);
+    });
+
+    test('It should respond with user returned', async () => {
+        const response = await request(app).get('/api/users/' + id)
+        expect(response.body.name).toBe('Johan Finndahl');
+        expect(response.statusCode).toBe(200);
+        console.log(response.body);
+    });
+
+
+    test('It should respond with user updated', async () => {
+        const response = await request(app).put('/api/users/' + id)
+            .send({ isAdmin: false });
+        expect(response.body.isAdmin).toBe(false);
+        expect(response.statusCode).toBe(200);
+        console.log(response.body);
+    });
+
+    test('It should respond with user deleted', async () => {
+        const response = await request(app).delete('/api/user/' + id)
+        //expect(response.body.isadmin).toBe(false);
+        expect(response.statusCode).toBe(200);
+        console.log(id);
     });
 });
